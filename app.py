d2d206f832a96d60d053e14498cf9e8a779afdac
sebb34 <bourrut@yahoo.com>
2024-12-26 08:26:08 +0100

    Update homepage to match site design system

--- app.py
+++ app.py
@@ -1014,4 +1014,4 @@ def reset_password(token):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
